1a4064baa238fb1417820a97ad8507c9283151f5
Pavankalyan477 <sarakantipavankalyan070@gmail.com>
2022-01-05 22:53:41 +0530

    student management

--- client/student/src/components/Navbar.jsx
+++ client/student/src/components/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import "./navbar.css"
+import pic from "./images/pic.jpg"
 
 export default function Navbar() {
+
+    const Redirect = async () =>
+    {
+        await(window.location.href="http://localhost:3003/auth/google")
+    }
     return (
-        <div>
-            <div><img src="" alt="" /></div>
-            <div><h3>STUDENT MANAGEMENT</h3></div>
-            <div><button>SIGNIN / SIGNUP</button></div>
+        <div className='navbar'>
+            <div><img src="https://www.pcquest.com/wp-content/uploads/2015/02/Schoolmgr.png" alt="pic" /></div>
+            <div><h2>STUDENT MANAGEMENT</h2></div>
+            <div onClick={Redirect} to=" "><button>SIGN IN/SIGN UP</button></div>
         </div>
     )
 }
