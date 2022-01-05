a278e535d8710399347379d83e5cba500c220e5a
shaista78 <shaistasulthana78@gmail.com>
2022-01-05 22:54:52 +0530

    updateing student details from

---
+++ client/student/src/components/StudentDetails.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Button, TextField } from '@mui/material'
+import { Box } from '@mui/material'
+
+export default function StudentDetails() {
+    return (
+        <div>
+             <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} >
+             <TextField
+            helperText="Please enter your name"
+            id="demo-helper-text-aligned"
+            label="Name"
+            />
+            <br/>
+             <TextField
+            helperText="Please enter your city"
+            id="demo-helper-text-aligned"
+            label="City"
+            />
+               <br/>
+             <TextField
+            helperText="Please enter your age"
+            id="demo-helper-text-aligned"
+            label="Age"
+            />
+               <br/>
+                <TextField
+            helperText="Please enter your education"
+            id="demo-helper-text-aligned"
+            label="Edducation"
+            />
+              <br/>
+             <TextField
+            helperText="Please enter your gender"
+            id="demo-helper-text-aligned"
+            label="Gender"
+            />
+               <br/>
+                <TextField
+            helperText="Please enter your conatct"
+            id="demo-helper-text-aligned"
+            label="Number"
+            />
+            <br/>
+            <Button variant="contained">Submit</Button>
+             </Box>
+        </div>
+    )
+}
+
